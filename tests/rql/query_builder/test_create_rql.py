b2617be7b4f4d994231e352d99ad25141d9f168c
Albert Sola <albert.sola@gmail.com>
2025-08-07 13:58:22 +0100

    MPT-12584 Fix use of private arguments in RQL Query constructor

--- tests/rql/query_builder/test_create_rql.py
+++ tests/rql/query_builder/test_create_rql.py
@@ -3,6 +3,7 @@ from mpt_api_client.rql.query_builder import RQLQuery
 
 def test_create():
     query = RQLQuery()
+
     assert query.op == RQLQuery.EXPRESSION
     assert query.children == []
     assert query.negated is False
@@ -10,13 +11,16 @@ def test_create():
 
 def test_create_with_field():
     query = RQLQuery("field")
+
     query.eq("value")
+
     assert query.op == RQLQuery.EXPRESSION
     assert str(query) == "eq(field,value)"
 
 
 def test_create_single_kwarg():
     query = RQLQuery(id="ID")
+
     assert query.op == RQLQuery.EXPRESSION
     assert str(query) == "eq(id,ID)"
     assert query.children == []
@@ -25,6 +29,7 @@ def test_create_single_kwarg():
 
 def test_create_multiple_kwargs():  # noqa: WPS218
     query = RQLQuery(id="ID", status__in=("a", "b"), ok=True)
+
     assert query.op == RQLQuery.AND
     assert str(query) == "and(eq(id,ID),in(status,(a,b)),eq(ok,true))"
     assert len(query.children) == 3
@@ -37,3 +42,38 @@ def test_create_multiple_kwargs():  # noqa: WPS218
     assert query.children[2].op == RQLQuery.EXPRESSION
     assert query.children[2].children == []
     assert str(query.children[2]) == "eq(ok,true)"
+
+
+def test_new_empty():
+    query = RQLQuery.new()
+
+    assert query.op == RQLQuery.EXPRESSION
+    assert query.children == []
+    assert query.negated is False
+
+
+def test_new_with_parameters():
+    project_rql = RQLQuery.new("project=rql")
+    status_not_done = RQLQuery.new("status=done", negated=True)
+
+    query = RQLQuery.new(
+        op=RQLQuery.AND,
+        children=[project_rql, status_not_done],
+    )
+
+    assert str(query) == "and(project=rql,not(status=done))"
+
+
+def test_new_with_set():
+    project_rql = RQLQuery.new("project=rql")
+    status_not_done = RQLQuery.new("status=done", negated=True)
+
+    query = RQLQuery.new(
+        op=RQLQuery.AND,
+        children={project_rql, status_not_done},
+    )
+
+    assert isinstance(query.children, list)
+    assert len(query.children) == 2
+    assert project_rql in query.children
+    assert status_not_done in query.children
