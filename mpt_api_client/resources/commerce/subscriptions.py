ee28092a141dda5f577b72202e3a8ae655c935f9
Albert Sola <albert.sola@gmail.com>
2025-09-03 11:48:11 +0100

    MPT-12845 Add subscriptions

---
+++ mpt_api_client/resources/commerce/subscriptions.py
@@ -0,0 +1,87 @@
+from mpt_api_client.http import (
+    AsyncCreateMixin,
+    AsyncDeleteMixin,
+    AsyncService,
+    CreateMixin,
+    DeleteMixin,
+    Service,
+)
+from mpt_api_client.models import Model, ResourceData
+
+
+class Subscription(Model):
+    """Subscription resource."""
+
+
+class SubscriptionsServiceConfig:
+    """Subscription service config."""
+
+    _endpoint = "/public/v1/commerce/subscriptions"
+    _model_class = Subscription
+    _collection_key = "data"
+
+
+class SubscriptionsService(  # noqa: WPS215
+    CreateMixin[Subscription],
+    DeleteMixin,
+    Service[Subscription],
+    SubscriptionsServiceConfig,
+):
+    """Subscription service."""
+
+    def render(self, resource_id: str) -> str:
+        """Render subscription template.
+
+        Args:
+            resource_id: Subscription resource ID
+
+        Returns:
+            Order template text in markdown format.
+        """
+        response = self._resource_do_request(resource_id, "GET", "render")
+        return response.text
+
+    def terminate(self, resource_id: str, resource_data: ResourceData) -> Subscription:
+        """Terminate subscription.
+
+        Args:
+            resource_id: Order resource ID
+            resource_data: Order resource data
+
+        Returns:
+            Subscription template text in markdown format.
+        """
+        return self._resource_action(resource_id, "POST", "terminate", json=resource_data)
+
+
+class AsyncSubscriptionsService(  # noqa: WPS215
+    AsyncCreateMixin[Subscription],
+    AsyncDeleteMixin,
+    AsyncService[Subscription],
+    SubscriptionsServiceConfig,
+):
+    """Async Subscription service."""
+
+    async def render(self, resource_id: str) -> str:
+        """Render subscription template.
+
+        Args:
+            resource_id: Subscription resource ID
+
+        Returns:
+            Order template text in markdown format.
+        """
+        response = await self._resource_do_request(resource_id, "GET", "render")
+        return response.text
+
+    async def terminate(self, resource_id: str, resource_data: ResourceData) -> Subscription:
+        """Terminate subscription.
+
+        Args:
+            resource_id: Order resource ID
+            resource_data: Order resource data
+
+        Returns:
+            Subscription template text in markdown format.
+        """
+        return await self._resource_action(resource_id, "POST", "terminate", json=resource_data)
--- tests/resources/commerce/test_commerce.py
+++ tests/resources/commerce/test_commerce.py
@@ -1,7 +1,13 @@
+import pytest
+
 from mpt_api_client.http import AsyncHTTPClient
 from mpt_api_client.resources.commerce import AsyncCommerce, Commerce
 from mpt_api_client.resources.commerce.agreements import AgreementsService, AsyncAgreementsService
 from mpt_api_client.resources.commerce.orders import AsyncOrdersService, OrdersService
+from mpt_api_client.resources.commerce.subscriptions import (
+    AsyncSubscriptionsService,
+    SubscriptionsService,
+)
 
 
 def test_commerce_init(http_client):
@@ -9,15 +15,6 @@ def test_commerce_init(http_client):
 
     assert isinstance(commerce, Commerce)
     assert commerce.http_client is http_client
-
-
-def test_commerce_orders_property(http_client):
-    commerce = Commerce(http_client=http_client)
-
-    orders_service = commerce.orders
-
-    assert isinstance(orders_service, OrdersService)
-    assert orders_service.http_client is http_client
 
 
 def test_commerce_orders_multiple_calls(http_client):
@@ -38,39 +35,33 @@ def test_async_commerce_init(async_http_client: AsyncHTTPClient):
     assert commerce.http_client is async_http_client
 
 
-def test_async_commerce_orders_property(async_http_client: AsyncHTTPClient):
-    commerce = AsyncCommerce(http_client=async_http_client)
-
-    orders_service = commerce.orders
-
-    assert isinstance(orders_service, AsyncOrdersService)
-    assert orders_service.http_client is async_http_client
-
-
-def test_async_commerce_orders_multiple_calls(async_http_client: AsyncHTTPClient):
-    commerce = AsyncCommerce(http_client=async_http_client)
-
-    orders_service = commerce.orders
-    orders_service_additional = commerce.orders
-
-    assert orders_service is not orders_service_additional
-    assert isinstance(orders_service, AsyncOrdersService)
-    assert isinstance(orders_service_additional, AsyncOrdersService)
-
-
-def test_async_agreements(async_http_client):
-    commerce = AsyncCommerce(http_client=async_http_client)
-
-    agreements = commerce.agreements
-
-    assert isinstance(agreements, AsyncAgreementsService)
-    assert agreements.http_client is async_http_client
-
-
-def test_agreements(http_client):
+@pytest.mark.parametrize(
+    ("attr_name", "expected"),
+    [
+        ("agreements", AgreementsService),
+        ("orders", OrdersService),
+        ("subscriptions", SubscriptionsService),
+    ],
+)
+def test_commerce_properties(http_client, attr_name, expected):
     commerce = Commerce(http_client=http_client)
 
-    agreements = commerce.agreements
+    service = getattr(commerce, attr_name)
 
-    assert isinstance(agreements, AgreementsService)
-    assert agreements.http_client is http_client
+    assert isinstance(service, expected)
+
+
+@pytest.mark.parametrize(
+    ("attr_name", "expected"),
+    [
+        ("agreements", AsyncAgreementsService),
+        ("orders", AsyncOrdersService),
+        ("subscriptions", AsyncSubscriptionsService),
+    ],
+)
+def test_async_commerce_properties(http_client, attr_name, expected):
+    commerce = AsyncCommerce(http_client=http_client)
+
+    service = getattr(commerce, attr_name)
+
+    assert isinstance(service, expected)
