56d24db101a90f6d8ba878a76dfddff87e6abc66
Albert Sola <albert.sola@gmail.com>
2025-10-06 11:56:12 +0100

    MPT-14077 Add notifications subscribers

---
+++ mpt_api_client/resources/notifications/subscribers.py
@@ -0,0 +1,42 @@
+from mpt_api_client.http import AsyncService, Service
+from mpt_api_client.http.mixins import (
+    AsyncCreateMixin,
+    AsyncDeleteMixin,
+    AsyncUpdateMixin,
+    CreateMixin,
+    DeleteMixin,
+    UpdateMixin,
+)
+from mpt_api_client.models import Model
+
+
+class Subscriber(Model):
+    """Subscriber resource."""
+
+
+class SubscribersServiceConfig:
+    """Subscribers service config."""
+
+    _endpoint = "/public/v1/notifications/subscribers"
+    _model_class = Subscriber
+    _collection_key = "data"
+
+
+class SubscribersService(  # noqa: WPS215
+    CreateMixin[Subscriber],
+    UpdateMixin[Subscriber],
+    DeleteMixin,
+    Service[Subscriber],
+    SubscribersServiceConfig,
+):
+    """Subscribers service."""
+
+
+class AsyncSubscribersService(  # noqa: WPS215
+    AsyncCreateMixin[Subscriber],
+    AsyncUpdateMixin[Subscriber],
+    AsyncDeleteMixin,
+    AsyncService[Subscriber],
+    SubscribersServiceConfig,
+):
+    """Async Subscribers service."""
