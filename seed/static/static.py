9c0c0b3dac479f7eac4774d6dab4ba2653183006
Albert Sola <albert.sola@gmail.com>
2025-12-15 16:32:14 +0000

    MPT-16430 Remove seeding

--- seed/static/static.py
+++
@@ -1,7 +0,0 @@
-import pathlib
-
-ICON = pathlib.Path(__file__).parent / "FIL-9920-4780-9379.png"
-
-LOGO = pathlib.Path(__file__).parent / "logo.png"
-
-PDF = pathlib.Path(__file__).parent / "empty.pdf"
