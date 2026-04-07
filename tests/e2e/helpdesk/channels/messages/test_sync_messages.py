e36b7b705ece0799cf9ec8c245d4dee3bdfb9b33
Lukasz Lancucki <lukasz.lancucki@softwareone.com>
2026-04-07 09:29:37 +0100

    test(e2e): update skip reasons for channel message tests

--- tests/e2e/helpdesk/channels/messages/test_sync_messages.py
+++ tests/e2e/helpdesk/channels/messages/test_sync_messages.py
@@ -8,7 +8,7 @@ from mpt_api_client.resources.helpdesk.chat_messages import ChatMessage
 pytestmark = [pytest.mark.flaky]
 
 
-@pytest.mark.skip(reason="Unskip after MPT-19124 completed")
+@pytest.mark.skip(reason="Unskip when MPT-19696 unblocked")
 def test_list_channel_messages(channel_messages_service):
     result = channel_messages_service.fetch_page(limit=1)
 
@@ -16,7 +16,7 @@ def test_list_channel_messages(channel_messages_service):
     assert all(isinstance(message, ChatMessage) for message in result)
 
 
-@pytest.mark.skip(reason="Unskip after MPT-19124 completed")
+@pytest.mark.skip(reason="Unskip when MPT-19696 unblocked")
 def test_list_channel_messages_not_found(mpt_ops, invalid_channel_id):
     with pytest.raises(MPTAPIError) as error:
         mpt_ops.helpdesk.channels.messages(invalid_channel_id).fetch_page(limit=1)
